c508b719711a37ce4b155e54e63d0cdb610dfb69
Anton <sdgfqasdf46@yandex.ru>
2021-09-14 21:19:38 +0300

    fix of Date

--- app/src/main/java/ru/chebertests/nasaphoto/view/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/chebertests/nasaphoto/view/PictureOfTheDayFragment.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -43,7 +43,7 @@ class PictureOfTheDayFragment : Fragment() {
         viewModel.getData().observe(viewLifecycleOwner, { renderData(it) })
 
         date.timeInMillis = System.currentTimeMillis()
-        viewModel.getPicture(dateFormatter(date))
+        viewModel.getPicture(null)
 
         return inflater.inflate(R.layout.picture_of_the_day_fragment, container, false)
     }
@@ -60,21 +60,7 @@ class PictureOfTheDayFragment : Fragment() {
 
         chip_group.setOnCheckedChangeListener { chip_group, position ->
             chip_group.findViewById<Chip>(position)?.let {
-                when(it.text) {
-                    this.beforeYesterday -> {
-                        val beforeYesterdayCal = Calendar.getInstance()
-                        beforeYesterdayCal.add(Calendar.DAY_OF_MONTH, -2)
-                        viewModel.getPicture(dateFormatter(beforeYesterdayCal))
-                    }
-                    this.yesterday -> {
-                        val yesterdayCal = Calendar.getInstance()
-                        yesterdayCal.add(Calendar.DAY_OF_MONTH, -1)
-                        viewModel.getPicture(dateFormatter(yesterdayCal))
-                    }
-                    this.today -> {
-                        viewModel.getPicture(dateFormatter(date))
-                    }
-                }
+                viewModel.getPicture(dateFormatter(it.text as String))
             }
         }
 
@@ -160,8 +146,20 @@ class PictureOfTheDayFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
-    private fun dateFormatter(date: Calendar) =
-        "${date.get(Calendar.YEAR)}-${date.get(Calendar.MONTH) + 1}-${date.get(Calendar.DAY_OF_MONTH)}"
+    private fun dateFormatter(day: String) : String {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = System.currentTimeMillis()
+        calendar.timeZone = TimeZone.getTimeZone("Etc/GMT-5")
+        when(day) {
+            beforeYesterday -> {
+                calendar.add(Calendar.DAY_OF_MONTH, -2)
+            }
+            yesterday -> {
+                calendar.add(Calendar.DAY_OF_MONTH, -1)
+            }
+        }
+        return DateFormat.format("yyyy-MM-dd",calendar.time).toString()
+    }
 
     companion object {
         fun newInstance() = PictureOfTheDayFragment()
